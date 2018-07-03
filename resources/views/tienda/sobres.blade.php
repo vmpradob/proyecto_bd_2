9bc35f4636a281031b30dc5c74f79309b7c895be
joseleon13 <35415699+joseleon13@users.noreply.github.com>
2018-07-03 00:22:20 -0400

    Avance vista sobre

--- resources/views/tienda/sobres.blade.php
+++ resources/views/tienda/sobres.blade.php
@@ -6,5 +6,28 @@
 @stop
 
 @section('content')
+    <div class="container-full-fluid">
+        <div class="row">
+        @php
+        $count = 0;
+        $count2 =0;
+        @endphp
+         @foreach($sobres as $sobre)
+            <div class="col-md-2">            
+            <img src="../uploads/imgUrl/{{$sobre->imgUrl}}" height="250" style="position: absolute; margin-top:{{($count2*320)+6}}px; margin-left: 15px">
+                <p style="position: absolute;color: black;font-size: 12px;margin-top: {{($count2*320)+250}}px;left:50%;">Precio: {{$sobre->precio}}</p>
+                <p style="position: absolute;color: black;font-size: 12px;margin-top: {{($count2*320)+270}}px;left:50%;">{{$sobre->nombre}}</p>
+            </a>       
 
+            </div>
+            @php
+            $count++; 
+            if($count == 6){ 
+                $count = 0; 
+                $count2++;
+            };
+            @endphp
+        @endforeach
+        </div>
+    </div>
 @endsection
